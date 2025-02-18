64598a8e2f799b46fb107330a4b18a9b4e963357
Modesto Echezarreta <echezarreta.m@gmail.com>
2025-02-18 13:34:53 -0600

    completed lissajous examples and exercises

---
+++ chapter1/exercises/exercise1-6/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{R: 0, G: 255, B: 0, A: 255}, // Green
+	color.RGBA{R: 255, G: 0, B: 0, A: 255}, // Red
+	color.RGBA{R: 0, G: 0, B: 255, A: 255}} // Blue
+
+const (
+	black_index = 0 // first color in palette
+	green_index = 1 // second color in palette
+	red_index   = 2 // second color in palette
+	blue_index  = 3 // second color in palette
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillationr revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // numer of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(rand.Intn(3)+1))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
