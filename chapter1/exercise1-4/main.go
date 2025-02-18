0c1b06de1c7299a6164372334420a17af06d110a
WellAckshually <echezarreta.m@gmail.com>
2025-02-17 20:33:50 -0600

    finished exercise1-4

--- chapter1/exercise1-4/main.go
+++ chapter1/exercise1-4/main.go
@@ -7,33 +7,47 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string]map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, "os.Stdin", counts)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				fmt.Fprintf(os.Stderr, "exercise1-4: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, arg, counts)
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for line, filenames := range counts {
+		fileCount := len(filenames)
+		if fileCount == 1 {
+			total := 0
+			for _, count := range filenames {
+				total += count
+			}
+			if total <= 1 {
+				continue
+			}
+		}
+		fmt.Printf("[Found in %d file(s)]\t%s\n", fileCount, line)
+		for name, count := range filenames {
+			fmt.Printf("\t%d hit(s) in %s\n", count, name)
 		}
 	}
 
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, filename string, counts map[string]map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		if counts[input.Text()] == nil {
+			counts[input.Text()] = make(map[string]int)
+		}
+		counts[input.Text()][filename]++
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
