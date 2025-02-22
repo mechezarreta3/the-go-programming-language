5ad8c1b691d30f5455fe28108519a796a2504df1
WellAckshually <echezarreta.m@gmail.com>
2025-02-22 13:45:32 -0600

    minor modifications to lissajous

--- chapter1/examples/lissajous/lissajous/lissajous.go
+++ chapter1/examples/lissajous/lissajous/lissajous.go
@@ -41,3 +41,29 @@ func Lissajous(out io.Writer) {
 	}
 	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
+
+func Lissajous(out io.Writer, cycles int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // numer of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	cycles = cycles              // number of complete x oscillationr revolutions
+	freq := rand.Float64() * 3.0 // relative frequency
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), black_index)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mechezarreta3/the-go-programming-language
 
 go 1.24.0
+
+require github.com/mechezarreta3/the-go-programming-language/chapter1/examples/lissajous v0.0.0-20250222183544-309f98a1e708
