18001af8d6b7908deed5ead2fae8c85249fc21cb
WellAckshually <echezarreta.m@gmail.com>
2025-03-02 19:02:47 -0600

    completed basename examples and started comma example

---
+++ chapter3/examples/comma/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+}
+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:] // 10000
+}
