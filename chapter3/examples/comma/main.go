91b53497fa24e0b39c1b9039494693dbdead2edb
WellAckshually <echezarreta.m@gmail.com>
2025-03-03 20:36:27 -0600

    completed comma example

--- chapter3/examples/comma/main.go
+++ chapter3/examples/comma/main.go
@@ -1,7 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf(" %s\n", comma(os.Args[i]))
+	}
 }
 
 // comma inserts commas in a non-negative decimal integer string.
